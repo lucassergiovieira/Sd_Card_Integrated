a71fa349876f6096e8cf681ed940cb7aa67981b8
lucassergiovieira <49031321+lucassergiovieira@users.noreply.github.com>
2019-07-04 01:41:13 -0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,15 @@
 #include "integration.hpp"
 
 int main() {
+    //############################
+    //ticker e timer pra gravar e ler semaforo
+    Ticker tsem(&status_semaforo,.5);
+    Timer  ndsem(&read_semaforo,20);
+
+
+
     event.rise(&begin);
     event.fall(&end);
     button();
 }
+
